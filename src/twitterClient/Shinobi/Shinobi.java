b64ec2f5de01367961b3a1b998f6522530b2dc3c
m.katsuda <prog.ten_fingers_2@ezweb.ne.jp>
2017-02-16 20:01:29 +0900

    Started developent by eclipse

---
+++ src/twitterClient/Shinobi/Shinobi.java
@@ -0,0 +1,22 @@
+package twitterClient.Shinobi;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Shinobi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Shinobi");
+	}
+
+}
