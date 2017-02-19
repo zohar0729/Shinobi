7175591f7b1b3b418828543f9eae6a434645e232
m.katsuda <zoharisikenuma@gmail.com>
2017-02-19 21:45:48 +0900

    removed myFrame.class

--- src/Shinobi/Shinobi.java
+++ src/Shinobi/Shinobi.java
@@ -1,9 +1,26 @@
 package Shinobi;
 
+import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
 import java.util.Date;
 
-public class Shinobi {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Shinobi extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7904313262072651538L;
+	Twitter twitter;
+	User user;
+	JLabel userName, userScreenName, userStatusCount, userLatestStatus;
 	public static String diffDate(Date dest){
 		long d = new Date().getTime() - dest.getTime();
 		String s = "";
@@ -23,8 +40,34 @@ public class Shinobi {
 		return s;
 	}
 	public static void main(String[] args) throws TwitterException{
-		myFrame f = new myFrame("Shinobi for Twitter");
+		Shinobi f = new Shinobi("Shinobi for Twitter");
 
 		f.setVisible(true);
 	}
+	Shinobi(String title) throws TwitterException{
+		twitter = new TwitterFactory().getInstance();
+		user = twitter.verifyCredentials();
+		
+		// ウィンドウの初期化
+		setTitle(title);
+		setSize(300, 300);
+		setLayout(new GridLayout());
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// 表示するラベルのインスタンス化および初期化
+		userName = new JLabel("ユーザー名:"+user.getName());
+		userScreenName = new JLabel("ユーザーID:"+user.getScreenName());
+		userStatusCount = new JLabel("ツイート数:"+user.getStatusesCount());
+		userLatestStatus = new JLabel(
+				"最新ツイート:"+Shinobi.diffDate(user.getStatus().getCreatedAt()));
+		
+		// コントロールのレイアウト
+		Container contentPane = getContentPane();
+		contentPane.add(userName, BorderLayout.NORTH);
+		contentPane.add(userScreenName, BorderLayout.NORTH);
+		contentPane.add(userStatusCount, BorderLayout.CENTER);
+		contentPane.add(userLatestStatus, BorderLayout.EAST);
+	}
+	
 }
---
+++ src/Shinobi/twitterConponents.java
@@ -0,0 +1,12 @@
+package Shinobi;
+
+import java.awt.Component;
+
+public class twitterConponents extends Component {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7626022013841348561L;
+
+}
